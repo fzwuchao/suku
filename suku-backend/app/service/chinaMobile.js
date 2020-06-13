2998b99a16159a4004b4e81d264138c0d15933ab
Colulu <youlan_xyz@163.com>
2020-06-13 18:10:32 +0800

    获取中国移动的token接口

--- suku-backend/app/service/chinaMobile.js
+++ suku-backend/app/service/chinaMobile.js
@@ -1,15 +1,26 @@
 'use strict';
 
+const moment = require('moment');
 const BaseService = require('../core/baseService');
 
-const CHINA_MOBILE_HOST = 'https://api.iot.10086.cn/v5';
-// const getTransid = () => {
-
-// }
 const commonParams = {
   transid: '',
   token: '',
 };
+
+// 8位序号, 如：00000001
+let index = 0;
+const getIndexStr = () => {
+  index += 1;
+  if (index > 99999999) index = 1;
+  return index.toString().padStart(8, '0');
+};
+
+// 当前时间字符串, 12小时制的
+const getNowStr = () => {
+  return moment().format('YYYYMMDDhhmmss');
+};
+
 const url = {
   get_token: '/ec/get/token',
   sim_base_info: '/ec/query/sim-basic-info',
@@ -18,44 +29,57 @@ const url = {
 };
 
 class ChinaMobileService extends BaseService {
-  async fetchData(url, data) {
-    return await this.ctx.curl(`${CHINA_MOBILE_HOST}${url}`, { data });
+  async fetchData(url, data, options) {
+    return await this.ctx.curl(url, {
+      data,
+      dataType: 'json',
+      ...options,
+    });
   }
 
   getResult(res) {
-    if (res.status === '0') {
-      return res.result;
+    if (res.status === 200) {
+      const resData = res.data;
+      if (resData.status === '0') {
+        return resData.result;
+      }
+      this.ctx.logger.error(res.message);
+      return [];
+
     }
-    this.ctx.logger.error(res.message);
+    this.ctx.logger.error(`【异常状态码】: ${res.status}`);
     return [];
   }
 
   /**
    * CMIOT_API25000-认证服务接口
    * token 过期时间为 1 小时
+   * @param {string} nameKey - token的key名
+   * @param {string} appid - appid
+   * @param {string} password -密码
+   * @param {string} hostAndVer - 主机和版本
    */
-  async getToken() {
-    const tokenName = 'onelinkToken';
-    const EXPIRE_TIME = 60 * 60 * 1000;
-    let token = await this.app.redis.get(tokenName);
+  async getToken(nameKey, appid, password, hostAndVer) {
+    // 设置超时时间为50分钟，避免产生时间差
+    const EXPIRE_TIME = 50 * 60 * 1000;
+    let token = await this.app.redis.get(nameKey);
 
     if (token) {
       return token;
     }
 
-    const data = {
-      appid: '',
-      password: '',
-      transid: '',
-    };
-    const res = await this.fetchData(url.get_token, data);
+    const res = await this.fetchData(`${hostAndVer}${url.get_token}`, {
+      appid,
+      password,
+      transid: `${appid}${getNowStr()}${getIndexStr()}`,
+    });
     const result = this.getResult(res);
     if (result.length > 0) {
       token = result[0].token;
     }
 
     const pipeline = await this.app.redis.pipeline();
-    await pipeline.set(tokenName, token).pexpire(tokenName, EXPIRE_TIME).exec((err, results)=> {});
+    await pipeline.set(nameKey, token).pexpire(nameKey, EXPIRE_TIME).exec((err, results)=> {});
 
     return token;
   }
