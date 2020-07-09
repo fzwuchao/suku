42c7400f1c7ae1be94dd392516e898f1ffb827a3
Colulu <youlan_xyz@163.com>
2020-07-09 23:31:19 +0800

    后端部署脚本修改

--- sh/deploy-backend.sh
+++ sh/deploy-backend.sh
@@ -4,7 +4,11 @@ rm -rf backend
 tar -xzvf backend.tar.gz -C ./
 
 backendContainer=suku-backend
-docker cp ./backend ${backendContainer}:/usr/
+# 重命名文件夹为suku-backend，与容器中的文件夹名称保持一致
+mv backend suku-backend
+docker cp ./suku-backend ${backendContainer}:/usr/
 docker restart ${backendContainer}
+# 文件夹名称改回之前的
+mv suku-backend backend
 
 echo '后端部署完成'
