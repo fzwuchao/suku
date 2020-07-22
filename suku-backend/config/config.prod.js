817892ea7fa86deef3e3d51a80beabbd6143776e
Colulu <youlan_xyz@163.com>
2020-07-22 16:51:47 +0800

    生产环境配置，默认使用config.default.js的配置，

--- suku-backend/config/config.prod.js
+++ suku-backend/config/config.prod.js
@@ -4,21 +4,21 @@
 
 'use strict';
 
-const path = require('path');
+// const path = require('path');
 
-const MYSQL_PARAMS = {
-  HOST: '47.115.75.162', // 'localhost',
-  PORT: 3306,
-  USER: 'suku', // 'root',
-  PASSWORD: 'ZY1305@OULAN', // 'root1234',
-  DATABASE: 'suku', // 'youlan_db',
-};
+// const MYSQL_PARAMS = {
+//   HOST: '47.115.75.162', // 'localhost',
+//   PORT: 3306,
+//   USER: 'suku', // 'root',
+//   PASSWORD: 'ZY1305@OULAN', // 'root1234',
+//   DATABASE: 'suku', // 'youlan_db',
+// };
 
-const REDIS_PARAMS = {
-  HOST: '47.115.75.162',
-  PORT: 6379,
-  PASSWORD: 'auth',
-};
+// const REDIS_PARAMS = {
+//   HOST: '47.115.75.162',
+//   PORT: 6379,
+//   PASSWORD: 'auth',
+// };
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
@@ -31,18 +31,18 @@ module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1589425408323_5421';
+  // config.keys = appInfo.name + '_1589425408323_5421';
   // add your middleware config here
   // 所有请求都会走checkToken（middleware/checkToken.js)
-  config.middleware = ['checkToken'];
-  config.jwt = {
-    secret: '123456', // 自定义 token 的加密条件字符串
-  };
+  // config.middleware = ['checkToken'];
+  // config.jwt = {
+  //   secret: '123456', // 自定义 token 的加密条件字符串
+  // };
 
-  config.cors = {
-    origin: '*',
-    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
-  };
+  // config.cors = {
+  //   origin: '*',
+  //   allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  // };
 
   // config.mysql = {
   //   // 单数据库信息配置
@@ -64,74 +64,80 @@ module.exports = appInfo => {
   //   agent: false,
   // };
 
-  config.sequelize = {
-    dialect: 'mysql',
-    // host
-    host: MYSQL_PARAMS.HOST,
-    // 端口号
-    port: MYSQL_PARAMS.PORT,
-    // 用户名
-    username: MYSQL_PARAMS.USER,
-    // 密码
-    password: MYSQL_PARAMS.PASSWORD,
-    // 数据库名
-    database: MYSQL_PARAMS.DATABASE,
-    timezone: '+08:00',
-    define: {
-      freezeTableName: true, // 强制model名与table名保持一致
-    },
-  };
+  // config.sequelize = {
+  //   dialect: 'mysql',
+  //   // host
+  //   host: MYSQL_PARAMS.HOST,
+  //   // 端口号
+  //   port: MYSQL_PARAMS.PORT,
+  //   // 用户名
+  //   username: MYSQL_PARAMS.USER,
+  //   // 密码
+  //   password: MYSQL_PARAMS.PASSWORD,
+  //   // 数据库名
+  //   database: MYSQL_PARAMS.DATABASE,
+  //   timezone: '+08:00',
+  //   define: {
+  //     freezeTableName: true, // 强制model名与table名保持一致
+  //   },
+  //   pool: {
+  //     max: 200,
+  //     min: 0,
+  //     // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
+  //     acquire: 100*1000,
+  //   },
+  // };
 
-  config.redis = {
-    client: {
-      port: REDIS_PARAMS.PORT, // Redis port
-      host: REDIS_PARAMS.HOST, // Redis host
-      password: REDIS_PARAMS.PASSWORD,
-      db: 0,
-    },
-  };
+  // config.redis = {
+  //   client: {
+  //     port: REDIS_PARAMS.PORT, // Redis port
+  //     host: REDIS_PARAMS.HOST, // Redis host
+  //     password: REDIS_PARAMS.PASSWORD,
+  //     db: 0,
+  //   },
+  // };
 
   // egg-multipart的参数配置
-  config.multipart = {
-    mode: 'file',
-    // 存放上传文件的目录
-    tmpdir: path.resolve(__dirname, '../tmp-file'),
-    fileExtensions: [
-      '.csv',
-      '.xlsx',
-      '.xls',
-    ],
-    fileSize: '50mb',
-  };
+  // config.multipart = {
+  //   mode: 'file',
+  //   // 存放上传文件的目录
+  //   tmpdir: path.resolve(__dirname, '../tmp-file'),
+  //   fileExtensions: [
+  //     '.csv',
+  //     '.xlsx',
+  //     '.xls',
+  //   ],
+  //   fileSize: '50mb',
+  // };
 
-  config.validate = {
-    convert: true, // 入参的数据类型自动转换
-    widelyUndefined: true, // 值为空字符串、NaN、Null转换成undefined
-  };
-  config.queue = {
-    client: {
-      queuePrefix: 'q',
-      redis: {
-        port: REDIS_PARAMS.PORT,
-        host: REDIS_PARAMS.HOST,
-        // auth: 'auth',
-        password: REDIS_PARAMS.PASSWORD,
-        db: 3,
-        options: {
-        },
-      },
-    },
-  };
+  // config.validate = {
+  //   convert: true, // 入参的数据类型自动转换
+  //   widelyUndefined: true, // 值为空字符串、NaN、Null转换成undefined
+  // };
+  // config.queue = {
+  //   client: {
+  //     queuePrefix: 'q',
+  //     redis: {
+  //       port: REDIS_PARAMS.PORT,
+  //       host: REDIS_PARAMS.HOST,
+  //       // auth: 'auth',
+  //       password: REDIS_PARAMS.PASSWORD,
+  //       db: 3,
+  //       options: {
+  //       },
+  //     },
+  //   },
+  // };
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
     // 配置CSRF token的默认字段名，前端发送的请求，都要带上这个字段
-    security: {
-      csrf: {
-        enable: false,
-      },
-    },
+    // security: {
+    //   csrf: {
+    //     enable: false,
+    //   },
+    // },
   };
 
   return {
