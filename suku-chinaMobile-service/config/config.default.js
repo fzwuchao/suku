2423641e39fe60a81ab516f9e722c7e3c4824630
Colulu <youlan_xyz@163.com>
2020-08-10 09:19:38 +0800

    初始化移动服务架构

---
+++ suku-chinaMobile-service/config/config.default.js
@@ -0,0 +1,36 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+const path = require('path');
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1596435267895_933';
+
+  // add your middleware config here
+  config.middleware = [];
+  config.logger = {
+    level: 'DEBUG',
+    consoleLevel: 'DEBUG',
+    dir: path.join(__dirname, '../logs', appInfo.name),
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
